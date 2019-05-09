53596b8fcb6d33bc887e292005cc074f5f2d887c
jamiepingEE <50338169+jamiepingEE@users.noreply.github.com>
2019-05-09 11:06:33 -0400

    Add files via upload

---
+++ ex1.2factorial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+        int N=0;
+        cout<<"enter positive number: ";  
+        cin>>N;
+        int factorial=1;
+        for(int i=1;i<=N;i++){
+                factorial=factorial*i;//we can also use fac=*i
+		//cout<<factorial<<endl;
+	}
+                cout<<"factorial is: "<<factorial<<endl;
+        
+}
+
