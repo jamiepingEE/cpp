6fda470845ce8dadca09d227fa00450f2cbd9c31
jamiepingEE <50338169+jamiepingEE@users.noreply.github.com>
2019-05-17 19:32:34 -0400

    Add files via upload

---
+++ ex2.3guessgame.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+	int seed=0;
+	int dice1=0,dice2=0;
+	cout<<"enter seed: ";
+	cin>>seed;
+	srand(seed);
+	dice1=1+(rand()%6);
+	dice2=1+(rand()%6);
+	int sum=dice1+dice2;
+	int guess=0;
+	int tries=-1;
+	int set=0;
+	if(sum%2==0){
+		tries=3;
+		set=tries;
+		do{
+			if(tries==set){
+				cout<<"sum is even, now guess. Enter first guess: ";
+				cin>>guess;
+			}
+			else{
+				cout<<"try again. ";
+				cin>>guess;
+			}
+			if(guess==sum){
+				cout<<"Congratulations, ";
+				break;
+			}
+			else{
+				cout<<"Incorrect. ";
+			}
+			tries--;
+		}while(tries!=0);
+	}
+	else{
+		tries=2;
+		set=tries;
+		do{
+			if(tries==set){
+				cout<<"sum is odd, now guess. Enter first guess: ";
+				cin>>guess;
+				if(guess>sum&&tries){
+					cout<<"Incorrect. Hint: Sum is less than "<<guess<<endl;
+				}
+				if(guess<sum&&tries){
+					cout<<"Incorrect. Hint: Sum is greater than "<<guess<<endl;
+				}
+			}
+			else{
+				cout<<"Try again. ";
+				cin>>guess;
+			}
+			if(guess==sum){
+				cout<<"Congratulations, ";
+				break;
+			}
+			else if(tries!=set){
+				cout<<"Incorrect. ";
+			}
+			tries--;
+		}while(tries!=0);
+	}
+	cout<<"sum is "<<dice1<<"+"<<dice2<<"="<<sum; //if program ends
+}
